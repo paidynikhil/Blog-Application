6e68f6989cbd5c598b2136e7c2df117c6089be55
Nikhil-BlueSparing <nikhilpaidy@bluesparing.com>
2024-08-31 22:20:50 +0530

    added route path  to navbar.

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import { Routes,Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
     return (
-        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Navbar/>}>
+            <Route path="sign-up" element={<h1>Sign up page</h1>}/>
+            <Route path="sign-in" element={<h1>Sign in page</h1>}/>
+            </Route>
+        </Routes>
     )
 }
 
